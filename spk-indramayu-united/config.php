1f3aaf0583cafd9f5f344f88328a34e1cdb90902
ikhlasulrahman <91641093+ikhlasulrahman@users.noreply.github.com>
2025-07-03 12:34:01 +0700

    Delete spk-indramayu-united directory

--- spk-indramayu-united/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$config["server"]='localhost';
-$config["username"]='root';
-$config["password"]='';
-$config["database_name"]='spk_indramayu_united';
--- spk-indramayu-united/home.php
+++
@@ -1,4 +0,0 @@
-<div class="page-header">
-    <h1 style="text-align: center; font-size: 32px; font-weight: bold; color: #2c3e50;">Beranda</h1>
-</div>
-
